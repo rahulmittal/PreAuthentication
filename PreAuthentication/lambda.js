3929ab045868f5b265bd37a22b4907c9e15313bc
rahulmittal <mittal_ims@yahoo.com>
2019-07-22 14:51:25 +0530

    save

--- PreAuthentication/lambda.js
+++ PreAuthentication/lambda.js
@@ -55,7 +55,7 @@ exports.handler = function (event, context, callback) {
 			}).promise().then((data) => {
 				console.log('PUT data success:' + JSON.stringify(data));
 				// Return to Amazon Cognito
-				return event;
+				context.done(null, event);
 			}).catch((err) => {
 				console.log("ERROR while put: " + JSON.stringify(err));
 				throw err;
@@ -66,9 +66,9 @@ exports.handler = function (event, context, callback) {
 			//reject(err);
 			event.response = err;
 			console.log("calling context done!- error");
-			//context.done(JSON.stringify(err));
+			context.done(JSON.stringify(err));
 			// Return error to Amazon Cognito
-			callback(error, event);			
+			//callback(error, event);			
 		});
 	}
 	//console.log("returning lambda...");
